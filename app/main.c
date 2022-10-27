d904826e374f2c76d5cf1d34b0967cf69443b7fa
dwij <dulanja33@gmail.com>
2022-10-28 00:43:57 +0200

    1st stage

--- app/main.c
+++ app/main.c
@@ -11,22 +11,22 @@ int main(int argc, char *argv[]) {
 	printf("Logs from your program will appear here!\n");
 
 	// Uncomment this block to pass the first stage
-	//
-	// char *command = argv[3];
-	// int child_pid = fork();
-	// if (child_pid == -1) {
-	//     printf("Error forking!");
-	//     return 1;
-	// }
-	//
-	// if (child_pid == 0) {
-	// 	   // Replace current program with calling program
-	//     execv(command, &argv[3]);
-	// } else {
-	// 	   // We're in parent
-	// 	   wait(NULL);
-	// 	   printf("Child terminated");
-	// }
+
+	 char *command = argv[3];
+	 int child_pid = fork();
+	 if (child_pid == -1) {
+	     printf("Error forking!");
+	     return 1;
+	 }
+
+	 if (child_pid == 0) {
+	 	   // Replace current program with calling program
+	     execv(command, &argv[3]);
+	 } else {
+	 	   // We're in parent
+	 	   wait(NULL);
+	 	   printf("Child terminated");
+	 }
 
 	return 0;
 }
